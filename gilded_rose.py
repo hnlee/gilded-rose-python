6a6a51fbf9686d768dee0094273ce9a146a4cfef
AgoloAbdarhmanTaha <abdarhman.taha@agolo.com>
2025-06-10 23:43:54 +0300

    latest chages

--- gilded_rose.py
+++ gilded_rose.py
@@ -8,6 +8,17 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
+            
+            if item.name == 'Conjured':
+                
+                if item.sell_in > 0:
+                    item.quality = max(item.quality - 2, 0)
+                else:
+                    item.quality = max(item.quality - 4, 0)
+                item.sell_in -= 1
+                continue
+            
+            
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
@@ -22,8 +33,10 @@ class GildedRose(object):
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
+                    
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
+            
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -6,11 +6,37 @@ from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
-        items = [Item("foo", 0, 0)]
+        items = [Item("Conjured", 5, 30), Item("Backstage passes to a TAFKAL80ETC concert", 7, 20)]
+        gilded_rose = GildedRose(items)
+        [print(item) for item in gilded_rose.items]
+        gilded_rose.update_quality()
+        print("After update:")
+        [print(item) for item in gilded_rose.items]
+        self.assertEqual(gilded_rose.items[0].quality, 28, "Conjured Support Isn't Working")
+        self.assertEqual(gilded_rose.items[1].quality, 22, "Backstage passes Support is broken")
+        gilded_rose.update_quality()
+        [print(item) for item in gilded_rose.items]
+        self.assertEqual(gilded_rose.items[0].quality, 26, "Conjured Support Isn't Working")
+        self.assertEqual(gilded_rose.items[1].quality, 24, "Backstage passes Support is broken")
+        
+    def test_all_current_cases(self):
+        items = [
+            Item("Aged Brie", 2, 0),
+            Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            Item("Sulfuras, Hand of Ragnaros", 0, 80),
+            Item("Conjured", 3, 6),
+            Item("Normal Item", 5, 10),
+            Item("Aged Brie", -2, 10),
+        ]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
-
+        
+        self.assertEqual(gilded_rose.items[0].quality, 1, "Aged Brie should increase in quality")
+        self.assertEqual(gilded_rose.items[1].quality, 21, "Backstage passes should increase in quality")
+        self.assertEqual(gilded_rose.items[2].quality, 80, "Sulfuras should not change quality")
+        self.assertEqual(gilded_rose.items[3].quality, 4, "Conjured items should decrease in quality")
+        self.assertEqual(gilded_rose.items[4].quality, 9, "Normal items should decrease in quality")
+        self.assertEqual(gilded_rose.items[5].quality, 11, "Aged Brie should still go up")
         
 if __name__ == '__main__':
     unittest.main()
